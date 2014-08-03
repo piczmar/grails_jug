7def4cbdc37fd96e99bd7a5983797ce12021477e
Marcin Piczkowski <piczmar@wp.pl>
2014-08-03 15:00:06 +0200

    GORM

--- grails-app/services/demo/MyService.groovy
+++ grails-app/services/demo/MyService.groovy
@@ -1,9 +1,25 @@
 package demo
 
-@groovy.transform.CompileStatic
+//@groovy.transform.CompileStatic
 class MyService{
 
 	String method(){
+
+		//def result = Quiz.findAllByNameLike('test%')
+		/*
+		def result = Quiz.withCriteria{
+			eq('name', 'test 1')
+		}
+		def result = Quiz.withCriteria{
+			'in'('name',['test 1', 'test 2'])
+		}
+		def result = Quiz.find("from Quiz q where q.name like :name",[name: 'test 1%'])
+
+		def result = Quiz.find(new Quiz(name: 'test 1'))*/
+
+
+		println result
+
 		return "Hello there"
 	}
 }
